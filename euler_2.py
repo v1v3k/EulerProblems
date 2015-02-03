bdd979e375811abd9202989db075dd1093c7496e
v1v3k <v1v3k.john@gmail.com>
2015-02-04 00:49:16 +0530

    euler2

--- euler_2.py
+++ euler_2.py
@@ -1,4 +1,13 @@
 __author__ = 'georgev'
 
-
-if __name__="__main__:"
+sum=0
+def fibo(a,b):
+    global sum;
+    print (b)
+    if b%2 is 0:
+        sum+=b
+    if  a+b <4000000:
+        fibo(b,a+b)
+if __name__ == "__main__":
+    fibo(1,1)
+    print (sum)
